c9aa58d8f7c70a9b178d59ecfac1c5518d98cab5
Eddie <edward.bawolek@gmail.com>
2025-11-18 15:34:45 -0700

    add turning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 VERSION =  pygame.version.ver
 
@@ -9,18 +10,18 @@ def main():
     fps = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = fps.tick(60) / 1000
-        print(dt)
-    # print(f"Starting Asteroids with pygame version: {VERSION}")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+        
 
 
 if __name__ == "__main__":
