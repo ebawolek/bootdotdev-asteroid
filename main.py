9604ae85afaa57a55735e7c4770e88e148395fe3
Eddie <edward.bawolek@gmail.com>
2025-11-18 15:58:32 -0700

    using groups

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ def main():
     pygame.init()
     fps = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = fps.tick(60) / 1000
         
